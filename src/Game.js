af980debda86cf91e184270749eb085e29c7590c
xiang.zhang <xiang.z.zhang@gmail.com>
2019-09-01 22:48:25 +0800

    code update

--- src/Game.js
+++ src/Game.js
@@ -3,15 +3,69 @@
  */
 
 import React from "react"
-import ReactDom from "react-dom"
-import {Link} from "_react-router-dom@5.0.1@react-router-dom";
+import btn from "./css/game.css"
 
-function Game() {
-       return (
-           <div>
-                  <h1>Hello World</h1>
-           </div>
-       )
+class Square extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+    render() {
+        return (
+            <button className="btn" onClick={() => {
+                this.setState( { value: this.state });
+            }}>
+                {this.state.value}
+            </button>
+        )
+    }
+}
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
+    handleClick(i)
+    {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+    renderSquare(i){
+        return(<Square value={this.state.squares[i]} />)
+    }
+    render() {
+        return(
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+    }
 }
 
+class Game extends React.Component{
+    render() {
+       return(<Board/>);
+    }
+}
 export default Game;
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,20 +2,22 @@ import React from "react";
 import './App.css';
 import {Link} from "react-router-dom";
 
-function Welcome() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src="logo.png" className="App-logo" alt="logo" />
-                <p>
-                    Have Fun !!!
-                </p>
-                <Link to="/Game">
-                    Welcome To Play Tic Tac Toe 2
-                </Link>
-            </header>
-        </div>
-    );
+class Welcome extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src="logo.png" className="App-logo" alt="logo" />
+                    <p>
+                        Have Fun !!!
+                    </p>
+                    <Link to="/Game">
+                        Welcome To Play Tic Tac Toe 2
+                    </Link>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default Welcome;
